e8e1569c6eddcc2ea20e621888168c66d4374ecd
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-07-29 20:29:52 +0530

    gallery-text modified

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -3,27 +3,10 @@ import { Box,Typography,ThemeProvider,Grid } from '@mui/material';
 import Gallery from './Gallery';
 
 import { 
-  headingTheme,
-  ServicesTextSecondaryColor,
-  subHeadingTypoTheme,
-  SkillsDetailsTypo 
+  headingTheme
 } from '../MaterialUI/typography';
 
 import { PhotoshootImages,weddingShootImages } from '../data';
-import img1 from '../assets/images/photoShoot/img1.jpg';
-import img2 from '../assets/images/photoShoot/img2.jpg';
-import img3 from '../assets/images/photoShoot/img3.jpg';
-import img4 from '../assets/images/photoShoot/img4.jpg';
-import img5 from '../assets/images/photoShoot/img5.jpg';
-
-import wImg1 from '../assets/images/weddingShoot/w-img1.jpg';
-import wImg2 from '../assets/images/weddingShoot/w-img2.jpg';
-import wImg3 from '../assets/images/weddingShoot/w-img3.jpg';
-import wImg4 from '../assets/images/weddingShoot/w-img4.jpg';
-import wImg5 from '../assets/images/weddingShoot/w-img5.jpg';
-
-
-
 
 
 const Work = () => {
@@ -52,37 +35,3 @@ const Work = () => {
 }
 
 export default Work;
-
-
-<Grid container spacing={1}>
-            <Grid item xs={6}>
-              <Box 
-              component='img'
-              src={img1}
-              className='photoshoot-img'/>
-            </Grid>
-            <Grid item xs={6}>
-              <Box 
-                component='img'
-                src={img2}
-                className='photoshoot-img'/>
-            </Grid>
-            <Grid item xs={12} height='300px'>
-              <Box 
-                component='img'
-                src={img4}
-                className='photoshoot-img'/>
-            </Grid>
-            <Grid item xs={6}>
-              <Box 
-                component='img'
-                src={img3}
-                className='photoshoot-img'/>
-            </Grid>
-            <Grid item xs={6}>
-              <Box 
-                component='img'
-                src={img5}
-                className='photoshoot-img'/>
-            </Grid>
-          </Grid>
